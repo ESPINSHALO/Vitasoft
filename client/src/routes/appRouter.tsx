2c94c29e602aaf3ed689bc4f441b512c2783b90c
espinshalosp <espinshalo@gmail.com>
2026-02-28 16:11:41 +0530

    Add profile route to app router

--- client/src/routes/appRouter.tsx
+++ client/src/routes/appRouter.tsx
@@ -5,6 +5,7 @@ import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
 import { TasksPage } from '../pages/TasksPage';
 import { ActivityPage } from '../pages/ActivityPage';
+import { ProfilePage } from '../pages/ProfilePage';
 import { RedirectIfAuth, RequireAuth } from './guards';
 
 export const router = createBrowserRouter([
@@ -42,6 +43,14 @@ export const router = createBrowserRouter([
         element: (
           <RequireAuth>
             <ActivityPage />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: 'profile',
+        element: (
+          <RequireAuth>
+            <ProfilePage />
           </RequireAuth>
         ),
       },
